3ef67698d67349b32c80d77733df4c8fd37a87a0
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:10:59 +0700

    Bai 16

---
+++ bai16/Program.cs
@@ -0,0 +1,30 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai16
+{
+    static void Main()
+    {
+        Console.Write("Nhap chuoi: ");
+        string str = Console.ReadLine();
+
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict[c] = 1;
+        }
+
+        Console.WriteLine("So lan xuat hien:");
+        foreach (var item in dict)
+        {
+            Console.WriteLine(item.Key + " : " + item.Value);
+        }
+    }
+}
