621e07f1d32702b71a5718af0987a43bb0f9cbcc
datthanh1408 <anhhungbodon@gmail.com>
2026-03-18 23:40:23 +0700

    Bai 3

---
+++ baii3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai3
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so: ");
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int max = list.Max();
+
+        Console.WriteLine("So lon nhat: " + max);
+    }
+}
