abb7d87da6d5d83b5e9b00b7982b9b42fb659a0a
datthanh1408 <anhhungbodon@gmail.com>
2026-03-18 23:57:21 +0700

    Bai 5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai5
+{
+    static void Main()
+    {
+        List<string> list = new List<string>() { "An", "Binh", "Cuong" };
+
+        Console.Write("Nhap ten can xoa: ");
+        string name = Console.ReadLine();
+
+        list.Remove(name);
+
+        Console.WriteLine("Danh sach sau khi xoa:");
+        foreach (var x in list)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
