b69fd32e190478421865cfee81436eb8139c06c6
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:01:18 +0700

    bai 7

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai7
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so: ");
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+
+        list.Sort();
+
+        Console.WriteLine("Danh sach tang dan:");
+        foreach (var x in list)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
