9c23e7da51c101fcc7d1fb7026080ce9476ef041
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:01:54 +0700

    bai 8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai8
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+        list.Reverse();
+
+        Console.WriteLine("Danh sach sau khi dao nguoc:");
+        foreach (var x in list)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
