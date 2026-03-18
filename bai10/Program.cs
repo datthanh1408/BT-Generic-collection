48cac0962cca43ee1342a6720cf7be0673ae6180
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:13:05 +0700

    Bai 10

---
+++ bai10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai10
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 5, 2, 8, 1, 3 };
+
+        int min = list.Min();
+
+        Console.WriteLine("Gia tri nho nhat: " + min);
+    }
+}
