c3e1b70f8ad4ea6f665f614336de54562cc396df
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:10:56 +0700

    Bai 15

---
+++ bai15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai15
+{
+    static void Main()
+    {
+        Dictionary<string, string> dict = new Dictionary<string, string>()
+        {
+            {"hello", "xin chao"},
+            {"book", "quyen sach"},
+            {"cat", "con meo"}
+        };
+
+        Console.Write("Nhap tu tieng Anh: ");
+        string key = Console.ReadLine().ToLower();
+
+        if (dict.ContainsKey(key))
+            Console.WriteLine("Nghia: " + dict[key]);
+        else
+            Console.WriteLine("Khong tim thay");
+    }
+}
