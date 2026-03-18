db8c1437a199b1bd6bab54bde3f549e71c3fd96c
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:11:02 +0700

    Bai 17

---
+++ bai17/Program.cs
@@ -0,0 +1,22 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai17
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 1, 2, 2, 3, 3, 3, 4 };
+
+        var max = list
+            .GroupBy(x => x)
+            .OrderByDescending(g => g.Count())
+            .First();
+
+        Console.WriteLine("So xuat hien nhieu nhat: " + max.Key);
+        Console.WriteLine("So lan: " + max.Count());
+    }
+}
