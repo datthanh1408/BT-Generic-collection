51dcaa315cc5d59455118000d36999b8586b0332
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:00:31 +0700

    Bai 6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai6
+{
+    static void Main()
+    {
+        List<string> list = new List<string>() { "An", "Binh", "Cuong" };
+
+        Console.Write("Nhap ten: ");
+        string name = Console.ReadLine();
+
+        if (list.Contains(name))
+            Console.WriteLine("Ton tai");
+        else
+            Console.WriteLine("Khong ton tai");
+    }
+}
