396d7991f27e6d370c690b58e2bf91ac5dc710c6
datthanh1408 <anhhungbodon@gmail.com>
2026-03-18 23:48:12 +0700

    Add files via upload
    bài4

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai4
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 1, 2, 4, 7, 8,10 };
+
+        int dem = 0;
+
+        foreach (int x in list)
+        {
+            if (x % 2 == 0)
+                dem++;
+        }
+
+        Console.WriteLine("So chan: " + dem);
+    }
+}
