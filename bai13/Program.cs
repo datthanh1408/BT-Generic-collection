467cd4cfbe0771c6571fb667664aa1738b8c058d
datthanh1408 <anhhungbodon@gmail.com>
2026-03-19 00:26:04 +0700

    Fix Bai 13

---
+++ bai13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Bai13
+{
+    static void Main()
+    {
+        List<Student> list = new List<Student>()
+        {
+            new Student{Id=1, Name="An"},
+            new Student{Id=2, Name="Binh"},
+            new Student{Id=3, Name="Cuong"},
+            new Student{Id=4, Name="dat"}
+        };
+
+        int count = list.Count;
+
+        Console.WriteLine("So sinh vien: " + count);
+    }
+}
