9c74a00f717daef680248ac41781be261e210182
datthanh1408 <anhhungbodon@gmail.com>
2026-03-18 23:35:29 +0700

    Bai 2

---
+++ Bai2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai2
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so thu " + (i + 1) + ": ");
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int sum = list.Sum();
+
+        Console.WriteLine("Tong = " + sum);
+    }
+}
