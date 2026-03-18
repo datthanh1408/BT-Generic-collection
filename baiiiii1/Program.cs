8e37f97c2752052921c1cef1bdf1d49d028e18d1
datthanh1408 <anhhungbodon@gmail.com>
2026-03-18 23:23:43 +0700

    Bai 1 - List int co ban

---
+++ baiiiii1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ten: Vo Van Thanh Dat
+// MSV: 22204
+
+using System;
+using System.Collections.Generic;
+
+class Bai1
+{
+    static void Main()
+    {
+        List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Danh sach:");
+        foreach (int x in list)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
